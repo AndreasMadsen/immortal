fa357ae656bdd56516339837c2fe12904aa56723
Andreas Madsen <amwebdk@gmail.com>
2012-02-26 12:10:52 +0100

    [refactor] add an execute file in native code
    this commit add an execute.cpp file there execute another process in a new session
    it also contain makefile used to compile the file.
    In order to simplify the workflow to users the src/out directory will contain binary
    executables.
    When installing this module 'node src/setup.js' will be executed and it will symlink
    the correct file depending on the OS to lib/executables

---
+++ src/execute.cpp
@@ -0,0 +1,44 @@
+#include <stdlib.h> /* exit */
+#include <unistd.h> /* fork, setsid */
+#include <stdio.h> /* printf */
+#include <signal.h> /* signal */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h> /* perror */
+
+int main(int argc, char *argv[]) {
+  char *filename = argv[0];
+  char **args = &argv[1];
+  int pid;
+
+  signal(SIGHUP, SIG_IGN);
+
+  pid = fork();
+  if (pid < 0) {
+    /* this is an error */
+    perror("fork()");
+    exit(1);
+  }
+  else if (pid > 0) {
+    /* this is the parent */
+    printf("{pid: %d}", pid);
+    exit(0);
+  }
+
+  /* this is the child */
+
+  if (setsid() < 0) {
+    perror("setsid()");
+    exit(1);
+  }
+  umask(022);
+
+  if (execvp(filename, args) < 0) {
+    perror("execvp()");
+    exit(1);
+  }
+  /* there's really no way to get here, ever. */
+  return 0;
+}
+
