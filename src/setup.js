3617a8d301237bfe81b7ee584340f2c0cc968d4f
Andreas Madsen <amwebdk@gmail.com>
2012-04-12 21:49:11 +0200

    [fix] allow installing again

--- src/setup.js
+++ src/setup.js
@@ -14,6 +14,7 @@
 
   var fs = require('fs');
   var path = require('path');
+  var existsFile = fs.exists || path.exists;
 
   var helpers = require('../lib/helpers.js');
 
@@ -47,23 +48,41 @@
   // get paths
   var from = path.join(helpers.root, 'src/out', 'execute-' + os);
   var to = helpers.executable('execute');
-  fs.symlink(from, to, 'file', function (error) {
 
-    // check for errors
-    if (error) {
-      process.stdout.write(red);
-      console.error(error.trace);
-      process.stdout.write(yellow);
-      console.error('an error occurred, please file an issue');
-      process.stdout.write(reset);
-
-      process.exit(1); return;
-    }
-
-    process.stdout.write(green);
-    console.log('installation complete');
+  // print error text
+  function printError(error) {
+    process.stdout.write(red);
+    console.error(error.trace);
+    process.stdout.write(yellow);
+    console.error('an error occurred, please file an issue');
     process.stdout.write(reset);
-    process.exit(0); return;
+    process.exit(1);
+  }
+
+  function createSymlink() {
+    fs.symlink(from, to, 'file', function (error) {
+      if (error) return printError(error);
+
+      // success
+      process.stdout.write(green);
+      console.log('installation complete');
+      process.stdout.write(reset);
+      process.exit(0); return;
+    });
+  }
+
+  // Create the symlink
+  existsFile(to, function (exists) {
+
+    // If file don't exist create a symlink
+    if (exists === false) return createSymlink();
+
+    // Remove file
+    fs.unlink(to, function (error) {
+      if (error) return printError(error);
+
+      createSymlink();
+    });
   });
 
 })();
