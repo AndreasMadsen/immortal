877df60ce9e6e01451f88070d83f2d4241b6388e
Andreas Madsen <amwebdk@gmail.com>
2012-04-13 18:42:56 +0200

    [test] add parent input tests

---
+++ test/simple/parent-input.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2012 Andreas Madsen
+ * MIT License
+ */
+
+var common = require('../common'),
+  immortal = require('immortal'),
+	vows = require('vows'),
+  assert = require('assert');
+
+vows.describe('immortal input check').addBatch({
+
+  'when callback is missing': {
+    topic: function () {
+      immortal.start(common.fixture('longlive.js'));
+    },
+    'it should throw because of missing callback': function (respons, err) {
+      assert.equal(err.message, 'callback is missing');
+      assert.isNull(respons);
+    }
+  },
+
+  'when executeing immortal with no options': {
+    topic: function () {
+      immortal.start(common.fixture('longlive.js'), this.callback);
+    },
+    'it should fail because of default monitor': function (respons, err) {
+      assert.ifError(err);
+      assert.equal(respons.message, 'monitor option .output must be a string');
+    }
+  },
+
+  'when daemon file do not exists': {
+    topic: function () {
+      immortal.start(common.fixture('missing.js'), {
+        strategy: 'development',
+        options: {
+          output: common.temp('should_not_exists')
+        }
+      },this.callback);
+    },
+    'it should fail because of missing file': function (respons, err) {
+      assert.ifError(err);
+      assert.equal(respons.message, 'file was not found (' + common.fixture('missing.js') + ')');
+    }
+  },
+
+  'when execute file do not exists': {
+    topic: function () {
+      immortal.start(common.fixture('longlive.js'), {
+        strategy: 'development',
+        exec: common.fixture('missing.js'),
+        options: {
+          output: common.temp('should_not_exists')
+        }
+      },this.callback);
+    },
+    'it should fail because of missing file': function (respons, err) {
+      assert.ifError(err);
+      assert.equal(respons.message, 'exec was not found (' + common.fixture('missing.js') + ')');
+    }
+  },
+
+  'when the option': {
+    'args is not an array': {
+      topic: function () {
+        immortal.start(common.fixture('missing.js'), {
+          strategy: 'development',
+          args: false
+        }, this.callback);
+      },
+      'it will fail': function (respons, err) {
+        assert.ifError(err);
+        assert.equal(respons.message, 'options.args must be an array');
+      }
+    },
+
+    'exec is not a string': {
+      topic: function () {
+        immortal.start(common.fixture('missing.js'), {
+          strategy: 'development',
+          exec: false
+        }, this.callback);
+      },
+      'it will fail': function (respons, err) {
+        assert.ifError(err);
+        assert.equal(respons.message, 'options.exec must be a string');
+      }
+    },
+
+    'env is not a string': {
+      topic: function () {
+        immortal.start(common.fixture('missing.js'), {
+          strategy: 'development',
+          env: null
+        }, this.callback);
+      },
+      'it will fail': function (respons, err) {
+        assert.ifError(err);
+        assert.equal(respons.message, 'options.env must be an object');
+      }
+    },
+
+    'strategy is not supported': {
+      topic: function () {
+        immortal.start(common.fixture('missing.js'), {
+          strategy: 'wrong'
+        }, this.callback);
+      },
+      'it will fail': function (respons, err) {
+        assert.ifError(err);
+        assert.equal(respons.message, 'options.strategy is not supported');
+      }
+    },
+
+    'monitor is not supported': {
+      topic: function () {
+        immortal.start(common.fixture('missing.js'), {
+          monitor: false
+        }, this.callback);
+      },
+      'it will fail': function (respons, err) {
+        assert.ifError(err);
+        assert.equal(respons.message, 'options.monitor must be a string or null');
+      }
+    }
+
+  }
+
+}).exportTo(module);
