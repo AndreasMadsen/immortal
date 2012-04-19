70624b48f5131fa0860417e4ea51f66a0b07068c
Andreas Madsen <amwebdk@gmail.com>
2012-04-19 21:30:05 +0200

    [test] allow 24 KB margin in error-loop testcase

--- test/simple/regress-error-loop.js
+++ test/simple/regress-error-loop.js
@@ -19,7 +19,7 @@ function startImmortal(callback) {
 
 var monitor = null;
 vows.describe('testing simultaneous error').addBatch({
-  
+
   'when starting immortal': {
     topic: function () {
       var self = this;
@@ -28,7 +28,7 @@ vows.describe('testing simultaneous error').addBatch({
         self.callback(error, prope);
       });
     },
-  
+
     'no errors should occurre in startup': function (error, monitor) {
       assert.ifError(error);
       assert.isNull(monitor.error);
@@ -36,7 +36,7 @@ vows.describe('testing simultaneous error').addBatch({
   }
 
 }).addBatch({
-  
+
   'when immortal group is ready': {
     topic: function () {
       var self = this;
@@ -45,39 +45,39 @@ vows.describe('testing simultaneous error').addBatch({
         self.callback(null, monitor);
       });
     },
-  
+
     'an error should occurre in this regression': {
       topic: function (monitor) {
         var self = this;
         var pids = common.copy(monitor.pid);
-  
+
         monitor.once('reconnect', function () {
           self.callback(null, monitor, pids);
         });
       },
-  
+
       'the old pump and child process should be dead': function (error, monitor, pids) {
         assert.ifError(error);
-        
+
         assert.equal(monitor.pid.daemon, pids.daemon);
-  
+
         assert.isNumber(pids.monitor);
         assert.isFalse(common.isAlive(pids.monitor));
-  
+
         assert.isNumber(pids.process);
         assert.isFalse(common.isAlive(pids.process));
       },
-  
+
       'the error should not contain something': function (error, monitor) {
         assert.ifError(error);
-        
+
         assert.equal(monitor.error.indexOf('eeeee'), 0);
-        assert.equal(monitor.error.length, 1047552);
+        assert.ok(monitor.error.length <= (1024*1024) && monitor.error.length > (1024*1000));
       }
     }
   }
 }).addBatch({
-  
+
   'when immortal stops': {
     topic: function () {
       var self = this;
@@ -90,16 +90,16 @@ vows.describe('testing simultaneous error').addBatch({
         });
       });
     },
-  
+
     'the immortal group should be dead': function (error, monitor, pid) {
       assert.ifError(error);
-  
+
       assert.isNumber(pid.daemon);
       assert.isFalse(common.isAlive(pid.daemon));
-  
+
       assert.isNumber(pid.monitor);
       assert.isFalse(common.isAlive(pid.monitor));
-  
+
       assert.isNull(pid.process);
     }
   }
